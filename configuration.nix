6dc8d5f01b4bcfa478c512b9c07b5d6e6d035d77
Andrii Olkhovych <hello@def4alt.com>
2024-12-29 10:16:31 +0100

    Move generate-nix file

--- configuration.nix
+++ configuration.nix
@@ -8,7 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ./services/generate-compose.nix
+      ./generate-compose.nix
     ];
 
   nix = {
