0b21e54d76baa0e164dd747480a20a2d438f3b22
Andrii Olkhovych <hello@def4alt.com>
2024-12-29 11:47:09 +0100

    Update nixos

--- configuration.nix
+++ configuration.nix
@@ -20,8 +20,6 @@
     '';
   };
 
-  nixpkgs.config.allowUnfree = true;
-
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
