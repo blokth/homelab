db986ed7ac547e1aeddd239f94419d53bf116bea
Andrii Olkhovych <hello@def4alt.com>
2024-12-29 10:36:45 +0100

    Simplify the script

--- generate-compose.nix
+++ generate-compose.nix
@@ -1,39 +1,25 @@
 { pkgs, lib, ... }:
 
 let
-  composeRoot = ./services;
-
-  # Recursively find all subdirectories containing docker-compose.yaml
-  findComposeProjects = dir:
+  findServices =
     let
-      paths = builtins.readDir dir;
+      paths = builtins.readDir ./services;
       isDir = name: paths.${name} == "directory";
-      hasComposeFile = folder: builtins.pathExists (dir + "/${folder}/docker-compose.yaml");
+      hasComposeFile = folder: builtins.pathExists ("./services/${folder}/docker-compose.yaml");
     in
-    map (folder: dir + "/${folder}")
-      (lib.filterAttrs (n: v: isDir n && hasComposeFile n) paths);
+    map (folder: "./services/${folder}")
+      (lib.filterAttrs (n: isDir n && hasComposeFile n) paths);
 
-  # Run compose2nix directly in project directory
-  generateNixFromCompose = projectDir: pkgs.runCommand "compose2nix-${builtins.baseNameOf projectDir}" { } ''
+  generateNix = projectDir: ''
     cd ${projectDir}
     ${pkgs.compose2nix} --project ${builtins.baseNameOf projectDir} --runtime docker
   '';
 
-  # Collect paths to docker-compose.nix files
-  generatedComposeFiles = map (projectDir:
-    let
-      composeFile = projectDir + "/docker-compose.nix";
-    in
-    if builtins.pathExists composeFile then
-      composeFile
-    else
-      null
-  ) (findComposeProjects composeRoot);
-
-  # Remove nulls from the list (if a service hasn't generated a nix file yet)
-  validImports = lib.filter (x: x != null) generatedComposeFiles;
+  generatedComposeFiles = builtins.filter (path:
+    builtins.pathExists (path + "/docker-compose.nix")
+  ) findServices;
 
 in {
-  imports = validImports;
-  system.activationScripts.generateCompose = lib.mkForce (lib.concatStringsSep "\n" (map generateNixFromCompose (findComposeProjects composeRoot)));
+  imports = generatedComposeFiles;
+  system.activationScripts.generateCompose = lib.mkForce (lib.concatStringsSep "\n" (map generateNix findServices));
 }
