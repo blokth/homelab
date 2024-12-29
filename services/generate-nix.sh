eef6dfedef4b3af5886bc0c821a1f87fccdd614f
Andrii Olkhovych <hello@def4alt.com>
2024-12-29 11:34:30 +0100

    Generate files

--- services/generate-nix.sh
+++ services/generate-nix.sh
@@ -2,9 +2,10 @@
 
 for dir in */; do
   if [ -d "$dir" ]; then
-    base_dir = $(basename "$dir")
+    base_dir=$(basename "$dir")
     echo "Generating docker-compose.nix in $base_dir"
-    cd "$base_dir"
-    nix run github:aksiksi/compose2nix -- --project "$base_dir" --runtime docker
+    cd "$dir"
+    nix run github:aksiksi/compose2nix -- --inputs docker-compose.yaml --project "$base_dir" --runtime docker
+    cd ..
   fi
 done
