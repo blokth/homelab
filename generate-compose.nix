d342a7fed6792f39b65df4b83309ae48b7edf5be
Andrii Olkhovych <hello@def4alt.com>
2024-12-29 10:38:57 +0100

    Use absolute path

--- generate-compose.nix
+++ generate-compose.nix
@@ -1,14 +1,9 @@
 { pkgs, lib, ... }:
 
 let
-  findServices =
-    let
-      paths = builtins.readDir ./services;
-      isDir = name: paths.${name} == "directory";
-      hasComposeFile = folder: builtins.pathExists ("./services/${folder}/docker-compose.yaml");
-    in
-    map (folder: "./services/${folder}")
-      (lib.filterAttrs (n: isDir n && hasComposeFile n) paths);
+  findServices = builtins.filter (path:
+    builtins.pathExists (path + "/docker-compose.yaml")
+  ) (builtins.attrValues (lib.filesystem.listDirectoriesRecursive ./services));
 
   generateNix = projectDir: ''
     cd ${projectDir}
@@ -20,6 +15,6 @@ let
   ) findServices;
 
 in {
-  imports = generatedComposeFiles;
+  imports = map (path: path + "/docker-compose.nix") generatedComposeFiles;
   system.activationScripts.generateCompose = lib.mkForce (lib.concatStringsSep "\n" (map generateNix findServices));
 }
